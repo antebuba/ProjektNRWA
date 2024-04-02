aa5c9515be7eefef771b05ca3b0ba66eb7d37f61
antebuba <ante.bubalo@fsre.sum.ba>
2024-04-02 13:14:41 +0200

    first commit

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+// web.php
+Route::resource('product', App\Http\Controllers\ProductController::class);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/products/{id}/edit', 'ProductController@edit')->name('Products.edit');
+
+Route::delete('/products/{id}', 'ProductController@destroy')->name('Products.destroy');
